f79cdf80fa4614aa459fa37133c43d520579c424
INotGreen <89376703+INotGreen@users.noreply.github.com>
2022-09-24 20:15:31 +0800

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
+using System.Security.Principal;
+namespace XieBroRAT
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            string dog = "goutougoutou"
+            Console.WriteLine($"疯狂星期四，VX转我50，给你完整版{dog}");
+        }
+    }
+
+}
